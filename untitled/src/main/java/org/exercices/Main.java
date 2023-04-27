8128b4b08f46c65f8af12920267bf9752b17ca28
LucasViniciusLima <lucaslimavzt@gmail.com>
2023-04-26 22:09:30 -0300

    first commit

---
+++ untitled/src/main/java/org/exercices/Main.java
@@ -0,0 +1,13 @@
+package org.exercices;
+
+/**
+ * $NAME responsável por
+ *
+ * @author lucas
+ * @since 2023-04-26
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
